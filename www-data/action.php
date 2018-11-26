ba8964762582d090a3f169f10519782e5096a0e3
James <168262+fatindeed@users.noreply.github.com>
2018-11-26 12:21:29 +0800

    修改标题

--- www-data/action.php
+++ www-data/action.php
@@ -36,7 +36,7 @@ file_put_contents($maskImgPath, $mask->getImageBlob());
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Invisible mask demo</title>
+    <title>Invisible Mask Demo</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
--- www-data/index.php
+++ www-data/index.php
@@ -12,7 +12,7 @@ foreach($files as $file) {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Invisible mask demo</title>
+    <title>Invisible Mask Demo</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -20,7 +20,7 @@ foreach($files as $file) {
   <body>
     <div class="container-fluid text-center">
       <form action="action.php" method="post" enctype="multipart/form-data" style="width: 100%;max-width: 360px;padding: 15px;margin: auto;">
-        <h1 class="h2">Invisible mask demo</h1>
+        <h1 class="h2">Invisible Mask Demo</h1>
         <div class="form-group row">
           <label for="image" class="col-4 col-form-label">Image</label>
           <div class="col-8">
